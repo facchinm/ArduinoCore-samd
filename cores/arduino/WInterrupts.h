46f85a0486e1c015bc4e21d8cd5bbd345f8ac48c
Martino Facchin <m.facchin@arduino.cc>
2017-09-07 15:17:19 +0200

    TEST: use dual lambda approach

--- cores/arduino/WInterrupts.h
+++ cores/arduino/WInterrupts.h
@@ -53,14 +53,17 @@ void detachInterrupt(uint32_t pin);
 }
 #endif
 
+#ifdef __cplusplus
 #ifndef W_INTERRUPTS_CPP
 #define W_INTERRUPTS_CPP
-#ifdef __cplusplus
 
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <functional>
 #include "wiring_private.h"
+
+#define __maybe_unused  __attribute__((unused))
 
 template <typename T>
 using voidTemplateFuncPtrParam =  void (*)(T param);
@@ -73,19 +76,14 @@ template<typename T> struct __container__ {
 // C++ only overloaded version of attachInterrupt function
 template<typename T> void attachInterrupt(uint8_t interruptNum, voidTemplateFuncPtrParam<T> userFunc, int mode, T& param) {
 
-  struct __container__<T> *cont = new __container__<T>();
-  cont->param = &param;
-  cont->function = &userFunc;
-
-  // TODO: check lambda scope
-  // TODO: add structure to delete(__container__) when detachInterrupt() is called
-  auto f = [](void* a) -> void
-  {
-    T* param = (T*)(((struct __container__<T>*)a)->param);
-    //(*((struct __container__<T>*)a)->function)(*param);
+  auto callback=[&](){
+    userFunc(param);
+  };
+  auto thunk=[](void* arg){ // note thunk is captureless
+    (*static_cast<decltype(callback)*>(arg))();
   };
 
-  attachInterruptParam(interruptNum, f, mode, cont);
+  attachInterruptParam(interruptNum, thunk, mode, &callback);
 }
 
 template<typename T> void attachInterrupt(uint8_t interruptNum, voidTemplateFuncPtrParam<T*> userFunc, int mode, T* param) {
