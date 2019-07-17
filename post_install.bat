1614e0ee75420b839f97189ba148c801ac54ba88
Martino Facchin <m.facchin@arduino.cc>
2019-07-17 14:05:00 +0200

    Install drivers only on Windows10

--- post_install.bat
+++ post_install.bat
@@ -1,17 +1,15 @@
 @echo off
-set ARGS=/SE /SW /SA
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-) ELSE (
-  drivers\dpinst-x86.exe %ARGS%
-)
-
-@echo off
 setlocal
 for /f "tokens=4-5 delims=[.] " %%i in ('ver') do @(if %%i==Version (set VERSION=%%j) else (set VERSION=%%i))
 if %VERSION% GEQ 10 (
+  REM uninstall arduino drivers on windows 10 - they just hurt composite USB enumeration 
+  if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+    drivers\dpinst-amd64.exe /PATH %cd%\drivers\prewin10 /U %ARGS%
+  ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+    drivers\dpinst-amd64.exe /PATH %cd%\drivers\prewin10 /U %ARGS%
+  ) ELSE (
+    drivers\dpinst-x86.exe /PATH %cd%\drivers\prewin10 /U %ARGS%
+  )
   exit /b 0
 )
 endlocal
